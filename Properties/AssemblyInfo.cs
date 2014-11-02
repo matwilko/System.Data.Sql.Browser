6c8a2e67f5efee97156ce1c2c14d00745132e3bb
Matt Wilkinson <matthew.wilkinson@gmail.com>
2014-11-02 20:05:08 +0000

    Initial project files

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("System.Data.Sql.Browser")]
+[assembly: AssemblyDescription("Lightweight and flexible interface to SQL Server Browser services")]
+[assembly: AssemblyCopyright("Copyright © Matthew Wilkinson 2014")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b62a08b1-9f1a-4980-ae51-3de0122a732f")]
